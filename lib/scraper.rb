14c14c0901deb7d5b64c5c8df9af725650ad9319
Cristina Lugo <cmlugoce@syr.edu>
2018-06-25 01:53:57 +0000

    editing scraaper.rb

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,24 +2,42 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-class Scraper
+class Wine
 
-doc = Nokogiri::HTML(open("https://www.winespectator.com/dailypicks/category/catid/1/page/1"))
-<<<<<<< HEAD
-doc_1 = Nokogiri::HTML(open("https://www.winespectator.com/dailypicks/category/catid/2"))
-doc_2 = Nokogiri::HTML(open("https://www.winespectator.com/dailypicks/category/catid/3"))
+
+
+
 doc_3 = Nokogiri::HTML(open("http://www.wines.com/wine-varietals/"))
 
-wine_name =doc.css('div.daily-wine-items h5 a').first.text
+def self.scrape_wine_price_less_15
+  wines = self.new
+doc = Nokogiri::HTML(open("https://www.winespectator.com/dailypicks/category/catid/1/page/1"))
+wine_name = doc.css('div.daily-wine-items h5 a').first.text
 wine_rating_price = doc.css('div.daily-wine-items h6').first.text
 wine_notes =  doc.css('div.daily-wine-items div.paragraph').children[0].text.strip
 wine_url = doc.css('div.daily-wine-items h5 a').first.attr('href')
-=======
 
-wine_name =doc.css('div.daily-wine-items h5 a').first.text
->>>>>>> aae8b20a0dacd97996147d08b425e1493986bfe9
+wines
+end
 
-binding.pry
+def self.scrape_wine_price_15_30
 
+doc_1 = Nokogiri::HTML(open("https://www.winespectator.com/dailypicks/category/catid/2"))
+wines = self.new
+wine_name = doc_1.css('div.daily-wine-items h5 a').first.text
+wine_rating_price= doc_1.css('div.daily-wine-items div.paragraph').children[0].text.strip
+wine_url = doc_1.css('div.daily-wine-items h5 a').first.attr('href')
 
+wines
+end
+
+def self.scrape_wine_price_30
+doc_2 = Nokogiri::HTML(open("https://www.winespectator.com/dailypicks/category/catid/3"))
+wines = self.new
+wine_name = doc_2.css('div.daily-wine-items h5 a').first.text
+wine_rating_price= doc_2.css('div.daily-wine-items div.paragraph').children[0].text.strip
+wine_url = doc_2.css('div.daily-wine-items h5 a').first.attr('href')
+
+wines
+end
 end
