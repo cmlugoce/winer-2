a5a9c0e56c5e43b019ddc5aa00f7fa770277fd7a
Cristina Lugo <cmlugoce@syr.edu>
2018-06-25 02:30:53 +0000

    change name of scraper.rb to wine.rb; finished the file

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,7 @@ require 'pry'
 
 class Wine
 
-
-
-
-doc_3 = Nokogiri::HTML(open("http://www.wines.com/wine-varietals/"))
-
+attr_accessor :name, :price, :url, :types, :description
 def self.scrape_wine_price_less_15
   wines = self.new
 doc = Nokogiri::HTML(open("https://www.winespectator.com/dailypicks/category/catid/1/page/1"))
@@ -24,9 +20,9 @@ def self.scrape_wine_price_15_30
 
 doc_1 = Nokogiri::HTML(open("https://www.winespectator.com/dailypicks/category/catid/2"))
 wines = self.new
-wine_name = doc_1.css('div.daily-wine-items h5 a').first.text
-wine_rating_price= doc_1.css('div.daily-wine-items div.paragraph').children[0].text.strip
-wine_url = doc_1.css('div.daily-wine-items h5 a').first.attr('href')
+wines.name = doc_1.css('div.daily-wine-items h5 a').first.text
+wines.rating_price= doc_1.css('div.daily-wine-items div.paragraph').children[0].text.strip
+wine.url = doc_1.css('div.daily-wine-items h5 a').first.attr('href')
 
 wines
 end
@@ -34,10 +30,33 @@ end
 def self.scrape_wine_price_30
 doc_2 = Nokogiri::HTML(open("https://www.winespectator.com/dailypicks/category/catid/3"))
 wines = self.new
-wine_name = doc_2.css('div.daily-wine-items h5 a').first.text
-wine_rating_price= doc_2.css('div.daily-wine-items div.paragraph').children[0].text.strip
-wine_url = doc_2.css('div.daily-wine-items h5 a').first.attr('href')
+wines.name = doc_2.css('div.daily-wine-items h5 a').first.text
+wines.rating_price= doc_2.css('div.daily-wine-items div.paragraph').children[0].text.strip
+wines.url = doc_2.css('div.daily-wine-items h5 a').first.attr('href')
 
 wines
 end
+
+def self.scrape_types
+doc_3 = Nokogiri::HTML(open("http://www.wines.com/wine-varietals/"))
+
+types = doc_3.css('h5')
+types each do |type|
+  wine = self.new
+  wine.name = type.css('h5').text
+type
+end
+end
+
+def self.scrape_wine_types_description
+doc_3 = Nokogiri::HTML(open("http://www.wines.com/wine-varietals/"))
+types = doc_3.css('p')
+types each do |type|
+  wine = self.new
+  wine.description = type.css('p').text
+  type
+end
+
+end
+
 end
